4ed2b6f3515002693c91fa13b59fb83b654943c9
EnzoVezzaro <enzo-vezzaro@live.it>
2023-11-25 19:06:27 -0400

    fix footer

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <footer className="relative bg-blueGray-200 pt-4 pb-6">
         <div
           className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
-          style={{ transform: "translateZ(0)" }}
+          style={{ transform: "translateZ(0)", backgroundColor: '#e2e8f0' }}
         >
           <svg
             className="absolute bottom-0 overflow-hidden"
